6e32d0f8574b82e7ed0c7b0595b061f56904971e
agnawak <91404742+agnawak@users.noreply.github.com>
2021-12-11 00:30:11 +0800

    Update Covid.py

--- Covid.py
+++ Covid.py
@@ -49,23 +49,3 @@ hospital_admission = pd.DataFrame(covid_19.groupby('Malaysia')['Admitted'].sum()
 fig4 = px.bar(hospital_admission, x = 'Admitted', y = hospital_admission.index, height = 600, color = 'Admitted', orientation = 'h',
              color_continuous_scale = ['paleturquoise','blue'], title = 'Hospital Admission')
 st.write(fig4)
-
-main_bg = "Lisa/Virus.jpg"
-main_bg_ext = "jpg"
-
-side_bg = "Lisa/Virus.jpg"
-side_bg_ext = "jpg"
-
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-    }}
-   .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
